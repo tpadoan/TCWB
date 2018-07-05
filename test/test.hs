2e83b8c5a171e26aa7c5272a5c99efd143e26b97
Tommaso Padoan <padoan@math.unipd.it>
2018-07-05 12:34:19 +0200

    Updated test main

--- test/test.hs
+++ test/test.hs
@@ -1,26 +1,18 @@
 module Main where
 
 import System.Environment
-import Control.Monad
-import System.Exit
-import Data.Maybe
-import qualified Data.Set as Set
-import qualified Data.ByteString.Lazy as BString
+import System.IO
+import Control.Exception (catchJust,AsyncException(UserInterrupt))
+import qualified Data.Map.Strict as Map
+import Commands
 import Model
 import Logic
-import MC
 
 main = do
   args <- getArgs
   let filePath:formula:l = args
 
-  json <- BString.readFile (filePath)
-  let mbnet = extractNetFromJSON json
-  when (isNothing mbnet) ((putStrLn "The input model is not correct.") >> exitFailure)
-  let Just net = mbnet
+  env1 <- loadIn filePath (Just "Sys") Map.empty
+  env2 <- defineProp "Prop" formula env1
 
-  let mblt = parseFormula formula
-  when (isNothing mblt) ((putStrLn "The input formula is not correct.") >> exitFailure)
-  let Just (treeF, resTok) = mblt
-
-  putStrLn (show (satisfies net treeF))
+  check "Prop" "Sys" env2
